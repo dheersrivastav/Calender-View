3caf6b8d98afb1a000dc0f095af2b4a396e03b21
Dheer srivastava <123939027+dheersrivastav@users.noreply.github.com>
2024-12-29 12:40:48 +0530

    All files upload and works

---
+++ src/components/common/Footer.js
@@ -0,0 +1,15 @@
+// components/common/Footer.js
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>© 2024 MY ERP System. All rights reserved.</p>
+        <p>Developed by Dheer</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
